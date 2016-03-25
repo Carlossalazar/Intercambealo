ad6a4ebbe196db528eb294be8fda6bb5417613b7
guega56 <guega56@hotmail.com>
2016-03-24 20:43:59 -0600

    prueba de crear usuarios
    no podemos

--- project/app/controllers/api/users_controller.rb
+++ project/app/controllers/api/users_controller.rb
@@ -88,7 +88,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :password, :password_confirmation, :firstname)
+      params.permit(:username, :password, :password_confirmation, :firstname)
     end
 end
 end
